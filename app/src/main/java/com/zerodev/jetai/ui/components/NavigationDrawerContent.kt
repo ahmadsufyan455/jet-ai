c863a05488f4d7036f8219bfa4243b3150b95c59
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2025-01-12 15:44:33 +0800

    add no chat history message if sessions empty

--- app/src/main/java/com/zerodev/jetai/ui/components/NavigationDrawerContent.kt
+++ app/src/main/java/com/zerodev/jetai/ui/components/NavigationDrawerContent.kt
@@ -2,9 +2,11 @@ package com.zerodev.jetai.ui.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -21,6 +23,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -53,18 +56,29 @@ fun NavigationDrawerContent(
             modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
         )
         HorizontalDivider(Modifier.padding(vertical = 8.dp))
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxHeight()
-                .padding(8.dp)
-                .weight(1f)
-        ) {
-            items(chatSessions) { chatSession ->
-                ChatSessionItem(chatSession) {
-                    chatViewModel.getChatsBySessionId(chatSession.sessionId)
-                    newSessionId = chatSession.sessionId
-                    scope.launch {
-                        drawerState.close()
+        if (chatSessions.isEmpty()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(stringResource(R.string.no_chat_history))
+            }
+        } else {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(8.dp)
+                    .weight(1f)
+            ) {
+                items(chatSessions) { chatSession ->
+                    ChatSessionItem(chatSession) {
+                        chatViewModel.getChatsBySessionId(chatSession.sessionId)
+                        newSessionId = chatSession.sessionId
+                        scope.launch {
+                            drawerState.close()
+                        }
                     }
                 }
             }
